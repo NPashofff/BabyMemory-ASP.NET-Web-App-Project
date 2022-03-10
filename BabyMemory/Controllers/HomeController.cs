6e00631a624107a2d066b9c36c3649f6f908fa45
niki pashofff <niki83@abv.bg>
2022-03-10 10:08:03 +0200

    Add NewseController

--- BabyMemory/Controllers/HomeController.cs
+++ BabyMemory/Controllers/HomeController.cs
@@ -23,66 +23,7 @@ namespace BabyMemory.Controllers
         {
             return View();
         }
-
-        public IActionResult News()
-        {
-            if (!User.Identity.IsAuthenticated)
-            {
-                return Redirect("/");
-            }
-
-            var models = context.News
-                .Where(x => x.IsActive == true)
-                .Select(x => new NewsViewModel
-                {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Description = x.Description,
-                    Picture = GlobalConstants.NewsLogo,
-                    IsActive = x.IsActive
-                }).ToArray();
-
-            return View(models);
-        }
-
-        public IActionResult AddNews()
-        {
-            //TODO: User Admin
-            if (!User.Identity.IsAuthenticated)
-            {
-                return Redirect("/");
-            }
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddNews(AddNewsViewModel model)
-        {
-            //TODO:Admin
-             if (!User.Identity.IsAuthenticated)
-            {
-                return Redirect("/");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return View("/Views/Shared/Error.cshtml");
-            }
-
-            var news = new News
-            {
-               Name = model.Name,
-                Description = model.Description
-            };
-
-            context.News.AddAsync(news);
-
-            context.SaveChangesAsync();
-
-            return Redirect("/Home/News");
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ BabyMemory/Controllers/NewsController.cs
@@ -0,0 +1,76 @@
+﻿using BabyMemory.Data;
+using BabyMemory.Data.Models;
+using BabyMemory.Models;
+using Microsoft.AspNetCore.Mvc;
+using SharedTrip.Shared;
+
+namespace BabyMemory.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult News()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            var models = _context.News
+                .Where(x => x.IsActive == true)
+                .Select(x => new NewsViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Picture = GlobalConstants.NewsLogo,
+                    IsActive = x.IsActive
+                }).ToArray();
+
+            return View(models);
+        }
+
+        public IActionResult AddNews()
+        {
+            //TODO: User Admin
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNews(AddNewsViewModel model)
+        {
+            //TODO:Admin
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("/Views/Shared/Error.cshtml");
+            }
+
+            var news = new News
+            {
+                Name = model.Name,
+                Description = model.Description
+            };
+
+            _context.News.Add(news);
+            _context.SaveChanges();
+
+            return Redirect("/News");
+        }
+    }
+}
