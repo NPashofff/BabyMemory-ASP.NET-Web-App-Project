411035e94ed0e592d339b32669cd15ed80f2d2af
niki pashofff <niki83@abv.bg>
2022-03-31 16:44:05 +0300

    fix  usings

--- BabyMemory/Controllers/MedicineController.cs
+++ BabyMemory/Controllers/MedicineController.cs
@@ -1,9 +1,9 @@
-﻿using BabyMemory.Core.Contracts;
-using BabyMemory.Infrastructure.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace BabyMemory.Controllers
+﻿namespace BabyMemory.Controllers
 {
+    using Core.Contracts;
+    using Infrastructure.Models;
+    using Microsoft.AspNetCore.Mvc;
+
     public class MedicineController : Controller
     {
         private readonly IMedicineService _medicineService;
@@ -16,15 +16,14 @@ namespace BabyMemory.Controllers
         public IActionResult Add(string childId)
         {
             ViewBag.CildId = childId;
+
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Add(MedicineAddViewModel model, string childId)
         {
-
             await _medicineService.AddMedicineAsync(model);
-
 
             return Redirect($"/Children/Profile?id={childId}");
         }
