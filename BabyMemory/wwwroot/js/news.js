febe1f074e1802dea56bae094a86b14a560566c5
niki pashofff <niki83@abv.bg>
2023-01-27 01:05:45 +0200

    fix news style

--- BabyMemory/wwwroot/js/news.js
+++ BabyMemory/wwwroot/js/news.js
@@ -29,20 +29,38 @@ getNews()
             const article = articles[i];
             const articleEl = document.createElement('article');
             articleEl.innerHTML = `
+
         <h2>${article.title}</h2>
         <p>${article.description}</p>
         <a href="${article.url}" target="_blank">Read more</a>
       `;
             const articleCard = document.createElement('div');
-            articleCard.className = "card col-12 col-sm-6 col-md-4 mb-2 d-flex align-items-stretch flex-column";
-            articleCard.style = "width: 18rem;";
+            //articleCard.className = "card col-12 col-sm-6 col-md-4 mb-2 d-flex align-items-stretch flex-column";
+            //articleCard.style = "width: 18rem;";
             articleCard.innerHTML = `
-    <img class="card-img-top " src="${article.urlToImage}" border="true" alt="Card image cap">
-    <div class="card-body">
-        <h5 class="card-title"><b>${article.title}<b/></h5>
-        <p class="card-text"><i>${article.description}<i/></p>
-        <a href="${article.url}" class="btn btn-primary">Read more</a>
-</div>
+              <div class="row">
+    <div class="col-md-12">
+        <div class="timeline">
+
+            <div>
+                <div class="card-body ">
+                    <div class="row mt-2">
+                        <div class="col-5">
+                            <img src="${article.urlToImage}" alt="user-avatar" class=" img-fluid">
+                        </div>
+                        <div class="col-7">
+                            <h1 class="lead"><b>${article.title}</b></h1>
+                            <p class="text-muted text-sm"><b>${article.description}</p>
+                            <a href="${article.url}" class="btn btn-light">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+            </div>
+        </div>
+    </div>
+</div>  
 `;
             content.append(articleCard);
         }
@@ -50,3 +68,20 @@ getNews()
     .catch((error) => {
         console.log(error);
     });
+
+//<div class="row">
+//    <div class="col-5">
+//    <img class="img" src="${article.urlToImage}">
+//    </div>
+//    <div class="col-5">
+//    <h3 class="timeline-header"><b>${article.title}</b></h3>
+
+//    <div class="timeline-body">
+//    <p>${article.description}</p>
+//    </div>
+//    <div class="timeline-footer">
+//    
+//    </div>
+//    </div>
+
+//    </div>
